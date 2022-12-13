3a09706fba04da5bcb69b786f457a0538cdacc06
JagadeeswaranG <jagadees.vg@gmail.com>
2022-12-13 13:12:45 +0530

    url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    api: "https://zomatoclone-server.onrender.com/",//http://localhost:3000
+    api: "https://zomatoclone-server.onrender.com",//http://localhost:3000
     storage_key:"zomato_app"
 }
